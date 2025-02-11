ada70993fdb02d83ed5eda9bfa0bae8ccdc47773
FarzanaHQTech <farzana.tech24@gmail.com>
2025-02-11 12:07:43 +0600

    warehouse

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('pages.dashboard-home');
 });
+
+Route::get('warehouse',function(){
+    return view('backend.Inventory.warehouse.warehouse');
+});
