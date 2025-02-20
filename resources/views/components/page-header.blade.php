d292af8aa2cd6670a83edada6d7b50a16bf1bf14
Rana <rana.net1995@gmail.com>
2025-02-20 14:53:12 +0600

    Update Crud

--- resources/views/components/page-header.blade.php
+++ resources/views/components/page-header.blade.php
@@ -44,8 +44,6 @@
         </li>
     </ul>
     <div class="page-btn">
-        <a {{ $attributes }} class="btn btn-added"><svg xmlns="http://www.w3.org/2000/svg" width="24"
-                height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
         <a {{$attributes}} class="btn btn-added"><svg xmlns="http://www.w3.org/2000/svg"
                 width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
                 stroke-linecap="round" stroke-linejoin="round" class="feather feather-plus-circle me-2">
