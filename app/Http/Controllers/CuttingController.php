f8ac697c7130eb302339016b651101e1b29409dd
delwarJnu11 <delwar999393@gmail.com>
2025-03-11 08:22:32 +0600

    calculate size based total used fabrics quantity

--- app/Http/Controllers/CuttingController.php
+++ app/Http/Controllers/CuttingController.php
@@ -8,6 +8,7 @@ use App\Models\Cutting;
 use App\Models\ProductionWorkOrder;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Crypt;
+use Illuminate\Support\Facades\DB;
 
 class CuttingController extends Controller
 {
@@ -42,7 +43,9 @@ class CuttingController extends Controller
             return redirect()->route('production-work-orders.index')->with('error', 'Work Order not found.');
         }
 
+        // Get work Order Id and total pieces
         $order_id = $workOrder->order_id;
+        $total_pieces = $workOrder->total_pieces;
 
         // Get BOM entry based on order_id
         $bom = Bom::where('order_id', $order_id)->first();
@@ -52,10 +55,38 @@ class CuttingController extends Controller
 
         $bom_id = $bom->id;
 
-        // Get total quantity_used from BOMDetails based on bom_id
-        $total_quantity_used = BomDetails::where('bom_id', $bom_id)->sum('quantity_used');
+        // Get order quanity based on Size
+        $sizeQuantities = DB::table('order_details')
+            ->where('order_id', $order_id)
+            ->select('size_id', DB::raw('SUM(qty) as total_qty'))
+            ->groupBy('size_id')
+            ->pluck('total_qty', 'size_id');
 
-        return view('pages.production.cutting.create', compact('work_order_id', 'order_id', 'bom_id', 'total_quantity_used'));
+        // Convert result into an array (size-wise quantity)
+        $sizesWithQty = $sizeQuantities->toArray();
+
+        // Get total quantity_used from BOMDetails based on bom_id
+        $Quantities = BomDetails::where('bom_id', $bom_id)
+            ->where('uom_id', 2)
+            ->select('size_id', DB::raw('SUM(quantity_used) as total_quantity_used'))
+            ->groupBy('size_id')
+            ->pluck('total_quantity_used', 'size_id');
+
+        // Convert to an array
+        $sizesWithUsedQty = $Quantities->toArray();
+
+        $totalFabricsUsed = 0;
+
+        foreach ($sizesWithQty as $sizeId => $orderQty) {
+            if (isset($sizesWithUsedQty[$sizeId])) {
+                $totalFabricsUsed += $orderQty * $sizesWithUsedQty[$sizeId];
+                break;
+            }
+        }
+
+        $total_quantity_wastage = BomDetails::where('bom_id', $bom_id)->sum('wastage');
+
+        return view('pages.production.cutting.create', compact('work_order_id', 'totalFabricsUsed', 'total_pieces'));
     }
 
     /**
--- resources/views/pages/production/cutting/create.blade.php
+++ resources/views/pages/production/cutting/create.blade.php
@@ -10,24 +10,12 @@
                 <form action="{{ route('cutting.store') }}" method="POST">
                     @csrf
                     <div class="col-md-12 mb-3">
-                        <label class="form-label">Order Number</label>
-                        <select name="order_id" class="form-select" id="order_dropdown" readonly>
-                            <option value="">Select Order</option>
-                            @forelse ($orders as $order)
-                                <option {{ $order_id == $order->id ? 'selected' : '' }} value="{{ $order->id }}">
-                                    {{ $order->order_number }} -
-                                    {{ $order->buyer->first_name . ' ' . $order->buyer->last_name }}
-                                </option>
-                            @empty
-                                <option value="">No order Found!</option>
-                            @endforelse
-                        </select>
-                        <x-input-error :messages="$errors->get('order_id')" class="mt-2" />
+                        <input type="text" name="work_order_id" id="work_order_id" value="{{ $work_order_id }}" hidden>
                     </div>
                     <div class="col-md-12 mb-3">
                         <label class="form-label">Cutting Section Status</label>
                         <select name="cutting_status" class="form-select" id="cutting_status">
-                            <option value="">Select Section Manager</option>
+                            <option value="">Select Section Status</option>
                             <option value="Pending">Pending</option>
                             <option value="In Progress">In Progress</option>
                             <option value="Completed">Completed</option>
@@ -36,22 +24,16 @@
                         <x-input-error :messages="$errors->get('cutting_status')" class="mt-2" />
                     </div>
                     <div class="col-md-12 mb-3">
-                        <label class="form-label">Work Order Status</label>
-                        <select name="work_order_status_id" class="form-select" id="work_order_status">
-                            <option value="">Select Work Order Status</option>
-                            @forelse ($workStatuses as $status)
-                                <option value="{{ $status->id }}">{{ $status->name }}</option>
-                            @empty
-                                <option value="">No work orderstatus Found!</option>
-                            @endforelse
-                        </select>
-                        <x-input-error :messages="$errors->get('work_order_status_id')" class="mt-2" />
-                    </div>
-                    <div class="col-md-12 mb-3">
                         <label for="total_pieces" class="form-label">Total Quantity</label>
                         <input class="form-control" type="text" name="total_pieces" id="total_pieces"
-                            value="{{ old('total_pieces', $totalQty) }}" placeholder="total Quantity..." />
+                            value="{{ old('total_pieces', $total_pieces) }}" placeholder="total Quantity..." />
                         <x-input-error :messages="$errors->get('total_pieces')" class="mt-2" />
+                    </div>
+                    <div class="col-md-12 mb-3">
+                        <label for="total_fabric_used" class="form-label">Total Fabrics Used</label>
+                        <input class="form-control" type="text" name="total_fabric_used" id="total_fabric_used"
+                            value="{{ old('total_fabric_used', $totalFabricsUsed) }}" placeholder="total Quantity..." />
+                        <x-input-error :messages="$errors->get('total_fabric_used')" class="mt-2" />
                     </div>
                     <div class="d-flex justify-content-end mt-3">
                         <button type="btn" id="create_btn" class="btn btn-primary">Create Work Order</button>
