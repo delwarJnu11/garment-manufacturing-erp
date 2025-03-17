dc7caa99fd56f167b17873c4973fc2befd35bf86
FarzanaHQTech <farzana.tech24@gmail.com>
2025-03-17 23:40:42 +0600

    PurchaseInvoiceControlle api for react

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\BomDetailsController;
 use App\Http\Controllers\Api\OrderDetailsController;
+use App\Http\Controllers\api\PurchaseInvoiceController;
 use App\Http\Controllers\Api\RawMaterialController;
 use App\Http\Controllers\Api\PurchaseOrderController;
 use App\Http\Controllers\Api\SalesInvoiceController;
@@ -24,8 +25,12 @@ Route::post('bom_details', [BomDetailsController::class, 'store']);
 Route::get('raw_material/{id}', [RawMaterialController::class, 'show']);
 // Route::get('order_details', [OrderDetailsController::class, 'index']);
 Route::get('order', [OrderDetailsController::class, 'index']);
+
 // purchaseOrder  Api
 Route::post('purchase', [PurchaseOrderController::class, 'store']);
 
 Route::post('salesinvoice', [SalesInvoiceController::class, 'store']);
-
+Route::get('suppliers', [PurchaseInvoiceController::class, 'supplier']);
+Route::get('warehouses', [PurchaseInvoiceController::class, 'warehouse']);
+Route::get('products', [PurchaseInvoiceController::class, 'product']);
+Route::post('saveReactpurchase', [PurchaseInvoiceController::class, 'saveReactpurchase']);
