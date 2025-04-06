c5babd0c5666080403f34baf5bca71a387c9f852
FarzanaHQTech <farzana.tech24@gmail.com>
2025-04-06 13:07:35 +0600

    user vueApi

---
+++ app/Http/Controllers/Api/Vue/UserController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers\Api\Vue;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+       $usersQuery = User::query();
+
+       if($request->search){
+            $usersQuery->where('name','like', '%'. $request->search.'%');
+       }
+       $users = $usersQuery->paginate(5);
+       return response()->json($users);
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
