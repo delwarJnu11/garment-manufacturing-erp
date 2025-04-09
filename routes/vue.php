2dfcdc172aef846fe2773ba7afcf9489862e30c0
FarzanaHQTech <farzana.tech24@gmail.com>
2025-04-09 13:19:53 +0600

    CategoryApi create

--- routes/vue.php
+++ routes/vue.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\Vue\AuthController;
+use App\Http\Controllers\Api\Vue\CategoryController;
 use App\Http\Controllers\Api\Vue\RolesController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,9 +16,9 @@ Route::post('logout', [AuthController::class, 'logout']);
 
 
 Route::apiResource('/roles', RolesController::class);
-// Route::apiResource('/roles', RolesController::class)->middleware('auth:api');
+
 
 // Farzana 
-// Route::apiResource('users', UserController::class);
+Route::apiResource('users', UserController::class);
 
-// Route::apiResource('categories', CategoryController::class);
+Route::apiResource('categories', CategoryController::class);
